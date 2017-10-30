99452f9a1f4fd53af9a20a0f082f52199a6c34d1
Daniele <irsooti@gmail.com>
2017-10-30 02:14:26 +0100

    Fix al response vuoto in teamspeak

--- services/teamspeak/teamspeak.controller.js
+++ services/teamspeak/teamspeak.controller.js
@@ -2,26 +2,32 @@ const service = require('./teamspeak.service')();
 
 
 module.exports = () => {
-    
+
     let modelViewClientList = (reply) => (response) => {
         let str = "👀 Guardo su Teamspeak... Adesso online ci sono: \n\n";
-        response.forEach(function (r) {
-            if (r.client_type != 1) {
-                str += '👉🏼 ' + r.client_nickname + ' ';
-                if (r.client_input_muted)
-                    str += '🙊'
-                if (r.client_output_muted)
-                    str += '🙉'
-                str += '\n'
-            }
-        }, this);
+        try {
+            response.forEach(function (r) {
+                if (r.client_type != 1) {
+                    str += '👉🏼 ' + r.client_nickname + ' ';
+                    if (r.client_input_muted)
+                        str += '🙊'
+                    if (r.client_output_muted)
+                        str += '🙉'
+                    str += '\n'
+                }
+            });
+        }
+        catch(err) {
+            console.log(err);
+            str += 'Non c\'è nessuno.';
+        }
 
-        reply(str);
-    }
+    reply(str);
+}
 
-    let showClients = (reply) => service.getClientList(modelViewClientList(reply))
+let showClients = (reply) => service.getClientList(modelViewClientList(reply))
 
-    return {
-        showClients: showClients
-    }
+return {
+    showClients: showClients
+}
 }
